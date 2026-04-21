d9f6273f8e397a003d495fd8ce4e9c1130ff84d7
DIR GEST. COMERCIAL <dirgestioncomercialkia@gmail.com>
2026-04-21 11:31:17 -0500

    analytics

---
+++ src/components/GoogleAnalytics.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import Script from 'next/script'
+
+const GA_ID = 'G-XNJRW8ZM6H'
+
+export function GoogleAnalytics() {
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_ID}');
+        `}
+      </Script>
+    </>
+  )
+}

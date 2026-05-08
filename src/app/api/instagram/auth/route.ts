65da594042c84bac4ecc4081c240bb92321d4745
unknown <ricaza81@gmail.com>
2026-05-07 21:26:59 -0500

    feat: selector de imagenes catalogo y banco IA en publicar Instagram

--- src/app/api/instagram/auth/route.ts
+++ src/app/api/instagram/auth/route.ts
@@ -2,20 +2,13 @@
 import { NextResponse } from 'next/server'
 
 export async function GET() {
-  const appId = process.env.META_APP_ID!
+  const igAppId = process.env.META_IG_APP_ID!
   const redirectUri = process.env.META_REDIRECT_URI!
 
-  const scope = [
-    'instagram_basic',
-    'instagram_content_publish',
-    'pages_read_engagement',
-    'pages_show_list',
-  ].join(',')
-
-  const url = new URL('https://www.facebook.com/v19.0/dialog/oauth')
-  url.searchParams.set('client_id', appId)
+  const url = new URL('https://www.instagram.com/oauth/authorize')
+  url.searchParams.set('client_id', igAppId)
   url.searchParams.set('redirect_uri', redirectUri)
-  url.searchParams.set('scope', scope)
+  url.searchParams.set('scope', 'instagram_business_basic,instagram_content_publish')
   url.searchParams.set('response_type', 'code')
 
   return NextResponse.redirect(url.toString())
